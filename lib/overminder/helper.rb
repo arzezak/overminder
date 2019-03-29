1de4f92d3cf8fe4ca2491cd377a489622e6a1a93
Ariel Rzezak <arzezak@gmail.com>
2019-03-29 11:48:12 -0300

    Check for overmind executable

--- lib/overminder/helper.rb
+++ lib/overminder/helper.rb
@@ -1,11 +1,17 @@
 module Overminder
   module Helper
     def with_procfile
+      abort("rake aborted!\nMissing `overmind` executable.") unless overmind_present?
+
       if File.exist?("Procfile.dev")
         yield
       else
-        abort "rake aborted!\nMissing `Procfile.dev`."
+        abort("rake aborted!\nMissing `Procfile.dev`.")
       end
+    end
+
+    def overmind_present?
+      File.exist?(`which overmind`.strip)
     end
   end
 end
